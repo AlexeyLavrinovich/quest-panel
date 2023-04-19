a865cf56aea616403222291a4208e2078f60313f
Alexey Lavrinovich <lavrinleha95@gmail.com>
2023-04-19 02:07:40 -0700

    Init commit

---
+++ src/main/java/com/questpanel/QuestPanelApplication.java
@@ -0,0 +1,13 @@
+package com.questpanel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestPanelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestPanelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/questpanel/QuestPanelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.questpanel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestPanelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
